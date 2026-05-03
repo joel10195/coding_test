efbd12324f0e70172078ca173bba3f4024112086
Kim Mincheol <joel19@naver.com>
2026-05-04 00:42:53 +0900

    [level 1] Title: 시저 암호, Time: 9.21 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,21 @@
+def solution(s, n):
+    lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    
+    idx = 0
+    result = ''
+        
+    
+    for i in s:
+        if i in lower:
+            idx = lower.index(i)
+            result = result + lower[idx + n]
+        elif i in upper:
+            idx = upper.index(i)
+            result = result + upper[idx + n]
+        
+        else:
+            result = result + ' '
+            
+    return result
